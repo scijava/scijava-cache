7262649b94d500540338c11f4e7d8ccb111df5ea
Curtis Rueden <ctrueden@wisc.edu>
2017-03-09 10:21:35 -0600

    Update license blurbs and add project license
    Accomplished using:
        mvn license:update-file-header license:update-project-license

--- src/main/java/org/scijava/cache/GuavaCacheService.java
+++ src/main/java/org/scijava/cache/GuavaCacheService.java
@@ -1,8 +1,8 @@
-/*
+/*-
  * #%L
- * SciJava Common shared library for SciJava software.
+ * SciJava cache implementation using Google Guava.
  * %%
- * Copyright (C) 2015 Board of Regents of the University of
+ * Copyright (C) 2015 - 2017 Board of Regents of the University of
  * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
  * Institute of Molecular Cell Biology and Genetics.
  * %%
